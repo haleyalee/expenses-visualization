078b1d200ef09c1b55ebc8c898dba3df2847e79f
Haley Lee <haley@Haleys-MacBook-Pro.local>
2024-10-26 17:06:59 -0400

    update to follow new database schema

--- express_server/server.js
+++ express_server/server.js
@@ -15,47 +15,73 @@ const app = express();
 app.use(cors());
 
 const authToken = process.env.NOTION_API_KEY;
-const dbId1 = process.env.NOTION_DATABASE_ID_1;
+const dbId = process.env.NOTION_DATABASE_ID;
 
 const notion = new Client({ auth: authToken });
 
+const months = ["2024-01-01", "2024-02-01", "2024-03-01", "2024-04-01", "2024-05-01", "2024-06-01", 
+  "2024-07-01", "2024-08-01", "2024-09-01", "2024-10-01", "2024-11-01", "2024-12-01"];
+
 app.get('/getExpenses', async(req, res) => {
   try {
-    const db = await notion.databases.retrieve({ database_id: dbId1 });
-    const dbExpenses = {
-      dbId: db.id,
-      title: db.title[0].text.content
-    }
-    
-    const dbRows = await notion.databases.query({
-      database_id: dbId1,
-      sorts: [
-        {
-          property: 'Date',
-          direction: 'ascending'
-        },
-        {
-          timestamp: 'created_time',
-          direction: 'ascending'
-        }
-      ]
-    });
+    const dbRows = await Promise.all(months.map(async (month, idx) => {          
+      const startDate = month;
+      const endDate = idx < months.length-1 ? months[idx+1] : "2025-01-01";
 
-    const { results } = dbRows;
-    // extract and transform critical information about expense
-    const expenses = results.map((e) => {
-      return {
-        name:  e.properties.Name.title[0].text.content,
-        category: e.properties.Category.multi_select.map((cat) => cat.name),
-        date: e.properties.Date.date.start,
-        amount: e.properties.Amount.number
-      }
-    });
-    
-    dbExpenses.expenses = expenses;
-    res.send(dbExpenses);
+      // filter database by month
+      const row = await notion.databases.query({
+        database_id: dbId,
+        filter: {
+          and: [
+            {
+              property: "Date",
+              date: {
+                "on_or_after": startDate
+              }
+            }, { 
+              property: "Date",
+              date: {
+                "before": endDate
+              }
+            }
+          ]
+        },
+        sorts: [
+          {
+            property: 'Date',
+            direction: 'ascending'
+          },
+          {
+            timestamp: 'created_time',
+            direction: 'ascending'
+          }
+        ]
+      });
+
+      const { results } = row;
+      
+      // for each month, extract and transform cirtical information about expense
+      const expenses = results.map((e) => {
+        return {
+          name:  e.properties.Source.title[0].plain_text,
+          category: e.properties.Category.multi_select[0]?.name,
+          date: e.properties.Date.date.start,
+          amount: e.properties.Amount.number
+        }
+      });
+
+      const monthExpenses = {
+        date: month.substring(0,7),
+        expenses: expenses
+      };
+
+      return monthExpenses;
+    }));
+
+    res.send(dbRows);
   } catch (error) {
     console.log(`Error getting database: ${error}`);
+    res.status(500).send("Error retrieving expenses data");
   }
 });
 
